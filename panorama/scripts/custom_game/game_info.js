b208de416f73bc7df67c0bd064f0a7498bc9b725
MitPeng <719505697@qq.com>
2020-06-16 19:49:44 +0800

    天赋技能面板移位

--- panorama/scripts/custom_game/game_info.js
+++ panorama/scripts/custom_game/game_info.js
@@ -1,29 +1,29 @@
-function ShowAllTalentAbilities(args) {
-    var parent = $("#talent_abilities_panel");
-    for (var i in args.Abilities) {
-        var abilityName = args.Abilities[i];
+// function ShowAllTalentAbilities(args) {
+//     var parent = $("#talent_abilities_panel");
+//     for (var i in args.Abilities) {
+//         var abilityName = args.Abilities[i];
 
-        var abilityPanel = $.CreatePanel("Panel", parent, "Ability" + abilityName);
-        abilityPanel.BLoadLayoutSnippet("AllTalentAbilities");
+//         var abilityPanel = $.CreatePanel("Panel", parent, "Ability" + abilityName);
+//         abilityPanel.BLoadLayoutSnippet("AllTalentAbilities");
 
-        abilityPanel.SetHasClass('CourierAbility', true);
+//         abilityPanel.SetHasClass('CourierAbility', true);
 
-        (function (panel, name) {
+//         (function (panel, name) {
 
-            panel.FindChildTraverse('ability_name').text = $.Localize("#DOTA_Tooltip_ability_" + abilityName);
+//             panel.FindChildTraverse('ability_name').text = $.Localize("#DOTA_Tooltip_ability_" + abilityName);
 
-            panel.FindChildTraverse("ability_image").abilityname = abilityName;
+//             panel.FindChildTraverse("ability_image").abilityname = abilityName;
 
-            panel.SetPanelEvent("onmouseover", function () {
-                $.DispatchEvent("DOTAShowAbilityTooltip", panel, name);
-            });
-            panel.SetPanelEvent("onmouseout", function () {
-                $.DispatchEvent("DOTAHideAbilityTooltip");
-            })
-        })(abilityPanel, abilityName);
-    }
-}
+//             panel.SetPanelEvent("onmouseover", function () {
+//                 $.DispatchEvent("DOTAShowAbilityTooltip", panel, name);
+//             });
+//             panel.SetPanelEvent("onmouseout", function () {
+//                 $.DispatchEvent("DOTAHideAbilityTooltip");
+//             })
+//         })(abilityPanel, abilityName);
+//     }
+// }
 
-(function () {
-    GameEvents.Subscribe("show_all_talent_abilities", ShowAllTalentAbilities);
-})();
+// (function () {
+//     GameEvents.Subscribe("show_all_talent_abilities", ShowAllTalentAbilities);
+// })();
