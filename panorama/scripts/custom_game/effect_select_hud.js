cc87696410fe3f2b8fa3f00e13c36cb288f732fc
MitPeng <719505697@qq.com>
2020-06-17 14:51:09 +0800

    特效选择面板

--- panorama/scripts/custom_game/effect_select_hud.js
+++ panorama/scripts/custom_game/effect_select_hud.js
@@ -20,3 +20,34 @@ function HideOrShow() {
 (function () {
     Hide();
 })();
+
+function ShowAllEffects(args) {
+
+    var parent = $("#effect_box_panel");
+    for (var i in args.Effects) {
+        var effectName = args.Effects[i];
+
+        var effectPanel = $.CreatePanel("Panel", parent, "Effect" + effectName);
+        effectPanel.BLoadLayoutSnippet("AllEffects");
+
+        effectPanel.SetHasClass('CourierAbility', true);
+
+        (function (panel, name) {
+
+            panel.FindChildTraverse('effect_name').text = $.Localize("#effect_" + effectName);
+
+            panel.FindChildTraverse("effect_image").SetImage("file://{resources}/images/custom_game/effects/" + effectName + ".png");
+            panel.FindChildTraverse('effect_select_button').SetPanelEvent("onactivate", function () {
+                GameEvents.SendCustomGameEventToServer("player_select_effect", {
+                    EffectName: name,
+                    PlayerID: Players.GetLocalPlayer()
+                });
+            });
+        })(effectPanel, effectName);
+    }
+
+}
+
+(function () {
+    GameEvents.Subscribe("show_all_effects", ShowAllEffects);
+})();
